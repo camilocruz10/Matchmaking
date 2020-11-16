e3b480c0720457755d26c2b100289f565a7713a4
Diego Arroyo Graciano <arroyodiego6@gmail.com>
2020-11-16 04:48:27 +0000

    Matchmaking

---
+++ matchmakingbackend/src/main/java/com/atomiclab/socialgamerbackend/controller/MatchmakingController.java
@@ -0,0 +1,36 @@
+package com.atomiclab.socialgamerbackend.controller;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import com.atomiclab.socialgamerbackend.domain.model.Matchmaking;
+import com.atomiclab.socialgamerbackend.service.MatchmakingService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/play")
+public class MatchmakingController {
+    @Autowired
+    MatchmakingService matchmakingService;
+
+    @PostMapping("/matchmaking/create")
+    public void create(@RequestBody Matchmaking matchmaking, @RequestHeader("X-Firebase-Auth") String token)
+            throws InterruptedException, ExecutionException {
+        matchmakingService.create(matchmaking, token);
+    }
+    @PostMapping("/matchmaking/match")
+    public List<Matchmaking> findMatch(@RequestBody Matchmaking matchmaking)
+            throws InterruptedException, ExecutionException {
+        return matchmakingService.findMatch(matchmaking);
+    }
+    @PostMapping("/matchmaking/delete")
+    public void delete(@RequestHeader("X-Firebase-Auth") String token) throws InterruptedException, ExecutionException {
+        matchmakingService.delete(token);
+    }
+}
