bb5845a3ed832eda59fb6298f9c1734837bc4d09
Diego Arroyo Graciano <arroyodiego6@gmail.com>
2020-11-24 22:45:57 +0000

    Deploy

--- matchmakingbackend/src/main/java/com/atomiclab/socialgamerbackend/service/impl/StorageServiceImpl.java
+++ matchmakingbackend/src/main/java/com/atomiclab/socialgamerbackend/service/impl/StorageServiceImpl.java
@@ -1,6 +1,8 @@
 package com.atomiclab.socialgamerbackend.service.impl;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import javax.annotation.PostConstruct;
 
@@ -15,9 +17,15 @@ public class StorageServiceImpl implements StorageService {
     private static StorageOptions storageOptions;
 
     @PostConstruct
-    private static void initializeStorage() throws Exception {
+    private static void initializeStorage() throws IOException {
         String projectId = "spring-course-c4e5a";
-        FileInputStream serviceAccount = new FileInputStream("spring-course-c4e5a-firebase-adminsdk-f0nr3-9a6cff1a6f.json");
+        FileInputStream serviceAccount;
+        try {
+            serviceAccount = new FileInputStream("spring-course-c4e5a-firebase-adminsdk-f0nr3-9a6cff1a6f.json");
+        } catch (FileNotFoundException e) {
+            serviceAccount = new FileInputStream("WEB-INF/classes/spring-course-c4e5a-firebase-adminsdk-f0nr3-9a6cff1a6f.json");
+            e.printStackTrace();
+        }
         storageOptions = StorageOptions.newBuilder().setProjectId(projectId)
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
     }
